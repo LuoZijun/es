5ae090e016715a9a557b56bb161e7bf5bd63ee6f
luozijun <luozijun.assistant@gmail.com>
2019-04-13 23:58:37 +0800

    Update bin/esc.rs

--- bin/esc.rs
+++ bin/esc.rs
@@ -18,17 +18,11 @@ use std::cell::{ Cell, RefCell, };
 fn main() {
     env::set_var("RUST_LOG", "ecmascript=trace,esc=trace");
     env_logger::init();
-    
-    // println!("{:?}", mem::size_of::<Rc<RefCell<ecmascript::vm::value::String>>>() );
-    // println!("{:?}", mem::size_of::<ecmascript::vm::value::String>() );
-    // println!("{:?}", mem::size_of::<ecmascript::vm::value::Symbol>() );
-    // println!("{:?}", mem::size_of::<ecmascript::vm::value::Object>() );
-    // println!("{:?}", mem::size_of::<ecmascript::vm::value::Number>() );
-    // println!("{:?}", mem::size_of::<ecmascript::vm::value::Null>() );
 
     let mut source = String::new();
     io::stdin().read_to_string(&mut source).unwrap();
 
-    // ecmascript::lexer::tokenize(&source);
-    ecmascript::parser::parse(&source);
+    ecmascript::lexer::tokenize(&source);
+    // ecmascript::parser::parse(&source);
+
 }
