45d87f625687097e81bb1b0145db4d68358c91b3
luozijun <luozijun.assistant@gmail.com>
2019-05-01 04:23:20 +0800

    Update src/lexer/span.rs

--- src/lexer/span.rs
+++ src/lexer/span.rs
@@ -22,7 +22,7 @@ pub struct Loc {
 
 impl fmt::Debug for LineColumn {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{:?}:{:?}", self.line, self.column)
+        write!(f, "{:?}:{:?}", self.line + 1, self.column)
     }
 }
 
@@ -35,5 +35,23 @@ impl fmt::Debug for Loc {
 impl fmt::Debug for Span {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{:?}..{:?}", self.start, self.end)
+    }
+}
+
+impl Default for LineColumn {
+    fn default() -> Self {
+        LineColumn { offset: 0, line: 0, column: 0, }
+    }
+}
+
+impl Default for Span {
+    fn default() -> Self {
+        Span { start: LineColumn::default(), end: LineColumn::default(), }
+    }
+}
+
+impl Default for Loc {
+    fn default() -> Self {
+        Loc { start: 0, end: 0, }
     }
 }
